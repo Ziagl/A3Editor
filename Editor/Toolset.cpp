7f32c31e8af62ddd75aaaaa705b37a9711b2790d
Ziagl <office@ziegelwanger-edv.at>
2020-05-20 12:49:47 +0200

    added reinitialize threads

--- Editor/Toolset.cpp
+++ Editor/Toolset.cpp
@@ -33,17 +33,30 @@ void Toolset::reinitializeGraph()
     // load graph data from Data.org folder
     InitializeGraph();
     Core::A3LegacyReader reader(logger);
-    reader.loadCountryFile(graph, "../Game/data/Data.org/LandDeut.sav"); // ### todo from config file
-    reader.loadCountryFile(graph, "../Game/data/Data.org/LandEngl.sav");
-    reader.loadCountryFile(graph, "../Game/data/Data.org/LandFran.sav");
-    reader.loadCountryFile(graph, "../Game/data/Data.org/LandHoll.sav");
-    reader.loadCountryFile(graph, "../Game/data/Data.org/LandItal.sav");
-    reader.loadCountryFile(graph, "../Game/data/Data.org/LandOest.sav");
-    reader.loadCountryFile(graph, "../Game/data/Data.org/LandPort.sav");
-    reader.loadCountryFile(graph, "../Game/data/Data.org/LandScho.sav");
-    reader.loadCountryFile(graph, "../Game/data/Data.org/LandSchw.sav");
-    reader.loadCountryFile(graph, "../Game/data/Data.org/LandSpan.sav");
-    reader.loadCountryFile(graph, "../Game/data/Data.org/LandTuer.sav");
+    // load data in parallel
+    std::thread t1(&Core::A3LegacyReader::loadCountryFile, &reader, graph, "../Game/data/Data.org/LandDeut.sav"); // ### todo from config file
+    std::thread t2(&Core::A3LegacyReader::loadCountryFile, &reader, graph, "../Game/data/Data.org/LandEngl.sav");
+    std::thread t3(&Core::A3LegacyReader::loadCountryFile, &reader, graph, "../Game/data/Data.org/LandFran.sav");
+    std::thread t4(&Core::A3LegacyReader::loadCountryFile, &reader, graph, "../Game/data/Data.org/LandHoll.sav");
+    std::thread t5(&Core::A3LegacyReader::loadCountryFile, &reader, graph, "../Game/data/Data.org/LandItal.sav");
+    std::thread t6(&Core::A3LegacyReader::loadCountryFile, &reader, graph, "../Game/data/Data.org/LandOest.sav");
+    std::thread t7(&Core::A3LegacyReader::loadCountryFile, &reader, graph, "../Game/data/Data.org/LandPort.sav");
+    std::thread t8(&Core::A3LegacyReader::loadCountryFile, &reader, graph, "../Game/data/Data.org/LandScho.sav");
+    std::thread t9(&Core::A3LegacyReader::loadCountryFile, &reader, graph, "../Game/data/Data.org/LandSchw.sav");
+    std::thread t10(&Core::A3LegacyReader::loadCountryFile, &reader, graph, "../Game/data/Data.org/LandSpan.sav");
+    std::thread t11(&Core::A3LegacyReader::loadCountryFile, &reader, graph, "../Game/data/Data.org/LandTuer.sav");
+    // wait till all threads are finised
+    t1.join();
+    t2.join();
+    t3.join();
+    t4.join();
+    t5.join();
+    t6.join();
+    t7.join();
+    t8.join();
+    t9.join();
+    t10.join();
+    t11.join();
 }
 
 // load current user defined data from filesystem
@@ -52,7 +65,7 @@ void Toolset::loadGraph()
     // load graph data from Data.org folder
     InitializeGraph();
     Core::A3LegacyReader reader(logger);
-    // load data parallelized
+    // load data in parallel
     std::thread t1(&Core::A3LegacyReader::loadCountryFile, &reader, graph, "../Game/data/Data.a3/LandDeut.sav"); // ### todo from config file
     std::thread t2(&Core::A3LegacyReader::loadCountryFile, &reader, graph, "../Game/data/Data.a3/LandEngl.sav");
     std::thread t3(&Core::A3LegacyReader::loadCountryFile, &reader, graph, "../Game/data/Data.a3/LandFran.sav");
