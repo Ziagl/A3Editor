e753e2357fdd7efcefd1f1f9de011e2e19bff049
Ziagl <office@ziegelwanger-edv.at>
2020-07-26 12:05:21 +0200

    added DialogNationalJersey

---
+++ Editor/DialogNationaljersey.cpp
@@ -0,0 +1,207 @@
+#include "DialogNationaljersey.h"
+
+DialogNationaljersey::DialogNationaljersey(wxWindow* parent, 
+    Toolset* const tools, 
+    const std::string selectedCountry,
+    wxWindowID id, 
+    const wxString& title, 
+    const wxPoint& pos, 
+    const wxSize& size, 
+    long style)
+    : wxDialog(parent, id, title, pos, size, style), tools(tools), m_selectedCountry(selectedCountry)
+{
+    /*if (!bBitmapLoaded) {
+        // We need to initialise the default bitmap handler
+        wxXmlResource::Get()->AddHandler(new wxBitmapXmlHandler);
+        wxC9ED9InitBitmapResources();
+        bBitmapLoaded = true;
+    }*/
+
+    wxBoxSizer* mainSizer = new wxBoxSizer(wxVERTICAL);
+    this->SetSizer(mainSizer);
+
+    wxFlexGridSizer* flexGridSizer17 = new wxFlexGridSizer(0, 2, 0, 0);
+    flexGridSizer17->SetFlexibleDirection(wxBOTH);
+    flexGridSizer17->SetNonFlexibleGrowMode(wxFLEX_GROWMODE_SPECIFIED);
+
+    mainSizer->Add(flexGridSizer17, 1, wxALL | wxEXPAND, WXC_FROM_DIP(5));
+
+    m_staticBitmap19 = new wxStaticBitmap(this, wxID_ANY, wxXmlResource::Get()->LoadBitmap(wxT("player")), wxDefaultPosition, wxDLG_UNIT(this, wxSize(-1, -1)), 0);
+
+    flexGridSizer17->Add(m_staticBitmap19, 0, wxALL, WXC_FROM_DIP(5));
+
+    wxFlexGridSizer* flexGridSizer21 = new wxFlexGridSizer(3, 1, 0, 0);
+    flexGridSizer21->SetFlexibleDirection(wxBOTH);
+    flexGridSizer21->SetNonFlexibleGrowMode(wxFLEX_GROWMODE_SPECIFIED);
+
+    flexGridSizer17->Add(flexGridSizer21, 1, wxALL | wxEXPAND, WXC_FROM_DIP(5));
+
+    wxBoxSizer* boxSizer23 = new wxBoxSizer(wxHORIZONTAL);
+
+    flexGridSizer21->Add(boxSizer23, 1, wxALL | wxEXPAND, WXC_FROM_DIP(0));
+
+    m_toggleButton29 = new wxToggleButton(this, wxID_ANY, _("Heimtrikot"), wxDefaultPosition, wxDLG_UNIT(this, wxSize(-1, -1)), 0);
+    m_toggleButton29->SetValue(true);
+
+    boxSizer23->Add(m_toggleButton29, 0, wxALL, WXC_FROM_DIP(5));
+
+    m_toggleButton31 = new wxToggleButton(this, wxID_ANY, _("Auswärtstrikot"), wxDefaultPosition, wxDLG_UNIT(this, wxSize(-1, -1)), 0);
+    m_toggleButton31->SetValue(false);
+
+    boxSizer23->Add(m_toggleButton31, 0, wxALL, WXC_FROM_DIP(5));
+
+    wxStaticBoxSizer* staticBoxSizer33 = new wxStaticBoxSizer(new wxStaticBox(this, wxID_ANY, _("Trikot Eigenschaften")), wxVERTICAL);
+
+    flexGridSizer21->Add(staticBoxSizer33, 1, wxALL | wxEXPAND, WXC_FROM_DIP(0));
+
+    wxFlexGridSizer* flexGridSizer53 = new wxFlexGridSizer(3, 1, 0, 0);
+    flexGridSizer53->SetFlexibleDirection(wxBOTH);
+    flexGridSizer53->SetNonFlexibleGrowMode(wxFLEX_GROWMODE_SPECIFIED);
+
+    staticBoxSizer33->Add(flexGridSizer53, 1, wxALL | wxEXPAND, WXC_FROM_DIP(5));
+
+    wxFlexGridSizer* flexGridSizer35 = new wxFlexGridSizer(0, 2, 0, 0);
+    flexGridSizer35->SetFlexibleDirection(wxBOTH);
+    flexGridSizer35->SetNonFlexibleGrowMode(wxFLEX_GROWMODE_SPECIFIED);
+
+    flexGridSizer53->Add(flexGridSizer35, 1, wxALL | wxEXPAND, WXC_FROM_DIP(0));
+
+    m_staticText37 = new wxStaticText(this, wxID_ANY, _("Trikotfarbe 1"), wxDefaultPosition, wxDLG_UNIT(this, wxSize(-1, -1)), 0);
+
+    flexGridSizer35->Add(m_staticText37, 0, wxALL, WXC_FROM_DIP(5));
+
+    wxArrayString m_choice39Arr;
+    m_choice39 = new wxChoice(this, wxID_ANY, wxDefaultPosition, wxDLG_UNIT(this, wxSize(-1, -1)), m_choice39Arr, 0);
+
+    flexGridSizer35->Add(m_choice39, 0, wxALL, WXC_FROM_DIP(5));
+
+    m_staticText41 = new wxStaticText(this, wxID_ANY, _("Trikotfarbe 2"), wxDefaultPosition, wxDLG_UNIT(this, wxSize(-1, -1)), 0);
+
+    flexGridSizer35->Add(m_staticText41, 0, wxALL, WXC_FROM_DIP(5));
+
+    wxArrayString m_choice43Arr;
+    m_choice43 = new wxChoice(this, wxID_ANY, wxDefaultPosition, wxDLG_UNIT(this, wxSize(-1, -1)), m_choice43Arr, 0);
+
+    flexGridSizer35->Add(m_choice43, 0, wxALL, WXC_FROM_DIP(5));
+
+    m_staticText45 = new wxStaticText(this, wxID_ANY, _("Hosenfarbe"), wxDefaultPosition, wxDLG_UNIT(this, wxSize(-1, -1)), 0);
+
+    flexGridSizer35->Add(m_staticText45, 0, wxALL, WXC_FROM_DIP(5));
+
+    wxArrayString m_choice47Arr;
+    m_choice47 = new wxChoice(this, wxID_ANY, wxDefaultPosition, wxDLG_UNIT(this, wxSize(-1, -1)), m_choice47Arr, 0);
+
+    flexGridSizer35->Add(m_choice47, 0, wxALL, WXC_FROM_DIP(5));
+
+    m_staticText49 = new wxStaticText(this, wxID_ANY, _("Stutzenfarbe"), wxDefaultPosition, wxDLG_UNIT(this, wxSize(-1, -1)), 0);
+
+    flexGridSizer35->Add(m_staticText49, 0, wxALL, WXC_FROM_DIP(5));
+
+    wxArrayString m_choice51Arr;
+    m_choice51 = new wxChoice(this, wxID_ANY, wxDefaultPosition, wxDLG_UNIT(this, wxSize(-1, -1)), m_choice51Arr, 0);
+
+    flexGridSizer35->Add(m_choice51, 0, wxALL, WXC_FROM_DIP(5));
+
+    wxFlexGridSizer* flexGridSizer55 = new wxFlexGridSizer(0, 2, 0, 0);
+    flexGridSizer55->SetFlexibleDirection(wxBOTH);
+    flexGridSizer55->SetNonFlexibleGrowMode(wxFLEX_GROWMODE_SPECIFIED);
+
+    flexGridSizer53->Add(flexGridSizer55, 1, wxALL | wxEXPAND, WXC_FROM_DIP(0));
+
+    m_radioButton57 = new wxRadioButton(this, wxID_ANY, _("neutral"), wxDefaultPosition, wxDLG_UNIT(this, wxSize(-1, -1)), 0);
+    m_radioButton57->SetValue(1);
+
+    flexGridSizer55->Add(m_radioButton57, 0, wxALL, WXC_FROM_DIP(5));
+
+    m_radioButton59 = new wxRadioButton(this, wxID_ANY, _("Brustring"), wxDefaultPosition, wxDLG_UNIT(this, wxSize(-1, -1)), 0);
+    m_radioButton59->SetValue(1);
+
+    flexGridSizer55->Add(m_radioButton59, 0, wxALL, WXC_FROM_DIP(5));
+
+    m_radioButton61 = new wxRadioButton(this, wxID_ANY, _("längs"), wxDefaultPosition, wxDLG_UNIT(this, wxSize(-1, -1)), 0);
+    m_radioButton61->SetValue(1);
+
+    flexGridSizer55->Add(m_radioButton61, 0, wxALL, WXC_FROM_DIP(5));
+
+    m_radioButton63 = new wxRadioButton(this, wxID_ANY, _("Ärmel"), wxDefaultPosition, wxDLG_UNIT(this, wxSize(-1, -1)), 0);
+    m_radioButton63->SetValue(1);
+
+    flexGridSizer55->Add(m_radioButton63, 0, wxALL, WXC_FROM_DIP(5));
+
+    m_radioButton65 = new wxRadioButton(this, wxID_ANY, _("quer"), wxDefaultPosition, wxDLG_UNIT(this, wxSize(-1, -1)), 0);
+    m_radioButton65->SetValue(1);
+
+    flexGridSizer55->Add(m_radioButton65, 0, wxALL, WXC_FROM_DIP(5));
+
+    m_radioButton67 = new wxRadioButton(this, wxID_ANY, _("Mittelstr."), wxDefaultPosition, wxDLG_UNIT(this, wxSize(-1, -1)), 0);
+    m_radioButton67->SetValue(1);
+
+    flexGridSizer55->Add(m_radioButton67, 0, wxALL, WXC_FROM_DIP(5));
+
+    m_radioButton69 = new wxRadioButton(this, wxID_ANY, _("Halb/Halb"), wxDefaultPosition, wxDLG_UNIT(this, wxSize(-1, -1)), 0);
+    m_radioButton69->SetValue(1);
+
+    flexGridSizer55->Add(m_radioButton69, 0, wxALL, WXC_FROM_DIP(5));
+
+    m_radioButton71 = new wxRadioButton(this, wxID_ANY, _("kariert"), wxDefaultPosition, wxDLG_UNIT(this, wxSize(-1, -1)), 0);
+    m_radioButton71->SetValue(1);
+
+    flexGridSizer55->Add(m_radioButton71, 0, wxALL, WXC_FROM_DIP(5));
+
+    m_radioButton73 = new wxRadioButton(this, wxID_ANY, _("Schulter"), wxDefaultPosition, wxDLG_UNIT(this, wxSize(-1, -1)), 0);
+    m_radioButton73->SetValue(1);
+
+    flexGridSizer55->Add(m_radioButton73, 0, wxALL, WXC_FROM_DIP(5));
+
+    m_radioButton75 = new wxRadioButton(this, wxID_ANY, _("schräg"), wxDefaultPosition, wxDLG_UNIT(this, wxSize(-1, -1)), 0);
+    m_radioButton75->SetValue(1);
+
+    flexGridSizer55->Add(m_radioButton75, 0, wxALL, WXC_FROM_DIP(5));
+
+    wxFlexGridSizer* flexGridSizer79 = new wxFlexGridSizer(0, 2, 0, 0);
+    flexGridSizer79->SetFlexibleDirection(wxBOTH);
+    flexGridSizer79->SetNonFlexibleGrowMode(wxFLEX_GROWMODE_SPECIFIED);
+
+    flexGridSizer53->Add(flexGridSizer79, 1, wxALL | wxEXPAND, WXC_FROM_DIP(0));
+
+    m_checkBox81 = new wxCheckBox(this, wxID_ANY, _("Ringelsocken"), wxDefaultPosition, wxDLG_UNIT(this, wxSize(-1, -1)), 0);
+    m_checkBox81->SetValue(false);
+
+    flexGridSizer79->Add(m_checkBox81, 0, wxALL, WXC_FROM_DIP(5));
+
+    wxBoxSizer* boxSizer96 = new wxBoxSizer(wxVERTICAL);
+
+    flexGridSizer21->Add(boxSizer96, 1, wxALL | wxEXPAND | wxALIGN_RIGHT, WXC_FROM_DIP(0));
+
+    m_buttonOk = new wxButton(this, wxID_ANY, _("OK"), wxDefaultPosition, wxDLG_UNIT(this, wxSize(-1, -1)), 0);
+
+    boxSizer96->Add(m_buttonOk, 0, wxALL, WXC_FROM_DIP(1));
+
+    m_buttonAbort = new wxButton(this, wxID_ANY, _("Abbrechen"), wxDefaultPosition, wxDLG_UNIT(this, wxSize(-1, -1)), 0);
+
+    boxSizer96->Add(m_buttonAbort, 0, wxALL, WXC_FROM_DIP(1));
+
+    SetName(wxT("MainDialogBaseClass"));
+    SetSize(wxDLG_UNIT(this, wxSize(-1, -1)));
+    if (GetSizer()) {
+        GetSizer()->Fit(this);
+    }
+    if (GetParent()) {
+        CentreOnParent(wxBOTH);
+    }
+    else {
+        CentreOnScreen(wxBOTH);
+    }
+/*#if wxVERSION_NUMBER >= 2900
+    if (!wxPersistenceManager::Get().Find(this)) {
+        wxPersistenceManager::Get().RegisterAndRestore(this);
+    }
+    else {
+        wxPersistenceManager::Get().Restore(this);
+    }
+#endif*/
+}
+
+DialogNationaljersey::~DialogNationaljersey()
+{
+}
---
+++ Editor/DialogNationaljersey.h
@@ -0,0 +1,68 @@
+#pragma once
+
+#include <wx/settings.h>
+#include <wx/xrc/xmlres.h>
+#include <wx/xrc/xh_bmp.h>
+#include <wx/dialog.h>
+#include <wx/iconbndl.h>
+#include <wx/artprov.h>
+#include <wx/sizer.h>
+#include <wx/statbmp.h>
+#include <wx/tglbtn.h>
+#include <wx/button.h>
+#include <wx/statbox.h>
+#include <wx/stattext.h>
+#include <wx/choice.h>
+#include <wx/arrstr.h>
+#include <wx/radiobut.h>
+#include <wx/checkbox.h>
+
+#include "Toolset.h"
+
+#ifdef WXC_FROM_DIP
+#undef WXC_FROM_DIP
+#endif
+#if wxVERSION_NUMBER >= 3100
+#define WXC_FROM_DIP(x) wxWindow::FromDIP(x, NULL)
+#else
+#define WXC_FROM_DIP(x) x
+#endif
+
+class DialogNationaljersey : public wxDialog
+{
+public:
+    DialogNationaljersey(wxWindow* parent, Toolset* const tools, const std::string selectedCountry, wxWindowID id = wxID_ANY, const wxString& title = _("My Dialog"), const wxPoint& pos = wxDefaultPosition, const wxSize& size = wxSize(-1, -1), long style = wxDEFAULT_DIALOG_STYLE | wxRESIZE_BORDER);
+    virtual ~DialogNationaljersey();
+
+protected:
+    wxStaticBitmap* m_staticBitmap19;
+    wxToggleButton* m_toggleButton29;
+    wxToggleButton* m_toggleButton31;
+    wxStaticText* m_staticText37;
+    wxChoice* m_choice39;
+    wxStaticText* m_staticText41;
+    wxChoice* m_choice43;
+    wxStaticText* m_staticText45;
+    wxChoice* m_choice47;
+    wxStaticText* m_staticText49;
+    wxChoice* m_choice51;
+    wxRadioButton* m_radioButton57;
+    wxRadioButton* m_radioButton59;
+    wxRadioButton* m_radioButton61;
+    wxRadioButton* m_radioButton63;
+    wxRadioButton* m_radioButton65;
+    wxRadioButton* m_radioButton67;
+    wxRadioButton* m_radioButton69;
+    wxRadioButton* m_radioButton71;
+    wxRadioButton* m_radioButton73;
+    wxRadioButton* m_radioButton75;
+    wxCheckBox* m_checkBox81;
+    wxButton* m_buttonOk;
+    wxButton* m_buttonAbort;
+
+private:
+    Toolset* tools = nullptr;
+    std::string m_selectedCountry;
+
+};
+
